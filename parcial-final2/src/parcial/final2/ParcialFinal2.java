36fa76cea84f47c875c5284a2c03d8a840cbc671
juanjo_zalasar@hotmail.com <Javier Rios Osorio@Dell>
2017-05-22 10:02:44 -0500

    inicio del parcial

---
+++ parcial-final2/src/parcial/final2/ParcialFinal2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial.final2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Javier Rios Osorio
+ */
+public class ParcialFinal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("ingrese un numero"));
+        int numero2 = Integer.parseInt(JOptionPane.showInputDialog("ingrese otro numero"));
+        
+        int suma = numero + numero2;
+        
+        JOptionPane.showMessageDialog(null,"el resultado es: "+suma);
+    }
+    
+}
